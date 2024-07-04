6a615b603c2d805ed751f2a7991778a77f947c6b
lingjhf <lingj.jhf@outlook.com>
2024-07-04 18:43:04 +0800

    添加规则

--- src/index.js
+++ src/index.js
@@ -1,39 +1,88 @@
-import tseslint from "typescript-eslint"
-import stylistic from "@stylistic/eslint-plugin"
-import stylisticTs from "@stylistic/eslint-plugin-ts"
-import parserTs from "@typescript-eslint/parser"
+import tseslint from 'typescript-eslint'
+import stylistic from '@stylistic/eslint-plugin'
+import parserTs from '@typescript-eslint/parser'
+import pluginVue from 'eslint-plugin-vue'
 
-export default [
+const defaultConfig = [
   stylistic.configs.customize({
     indent: 2,
-    quotes: "single",
+    quotes: 'single',
     semi: false,
     jsx: true,
   }),
   ...tseslint.config(
     ...tseslint.configs.stylistic,
-  ),
-  {
-    plugins: {
-      "@stylistic/ts": stylisticTs,
+    ...tseslint.configs.strict,
+    {
+      plugins: {
+        '@stylistic': stylistic,
+      },
+      languageOptions: {
+        parser: parserTs,
+      },
+      rules: {
+        'no-var': 'error',
+        'prefer-const': 'error',
+        'prefer-rest-params': 'error',
+        'prefer-spread': 'error',
+        'no-undef-init': 'error',
+
+        '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+        'no-extra-semi': 'off',
+        '@typescript-eslint/no-extra-semi': 'error',
+        '@stylistic/member-delimiter-style': [
+          'error',
+          {
+            multiline: {
+              delimiter: 'comma',
+              requireLast: true,
+            },
+            singleline: {
+              delimiter: 'comma',
+              requireLast: true,
+            },
+          },
+        ],
+
+      },
     },
+  ),
+]
+
+export const vueConfig = [
+  ...defaultConfig,
+  ...pluginVue.configs['flat/strongly-recommended'],
+  {
+    files: ['*.vue', '**/*.vue'],
     languageOptions: {
-      parser: parserTs,
+      parserOptions: {
+        parser: '@typescript-eslint/parser',
+      },
     },
     rules: {
-      "@stylistic/member-delimiter-style": [
-        "error",
+      'vue/multi-word-component-names': 'off',
+      'vue/html-quotes': ['error', 'single', { avoidEscape: true }],
+      'vue/attributes-order': [
+        'error',
         {
-          multiline: {
-            delimiter: "comma",
-            requireLast: true,
-          },
-          singleline: {
-            delimiter: "comma",
-            requireLast: true,
-          },
+          order: [
+            'DEFINITION',
+            'LIST_RENDERING',
+            'CONDITIONALS',
+            'RENDER_MODIFIERS',
+            'GLOBAL',
+            ['UNIQUE', 'SLOT'],
+            'TWO_WAY_BINDING',
+            'OTHER_DIRECTIVES',
+            'OTHER_ATTR',
+            'EVENTS',
+            'CONTENT',
+          ],
+          alphabetical: true,
         },
       ],
     },
   },
 ]
+
+export default defaultConfig
